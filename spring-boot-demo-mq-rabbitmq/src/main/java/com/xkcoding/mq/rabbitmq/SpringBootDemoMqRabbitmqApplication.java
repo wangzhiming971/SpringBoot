12e4be156a3b8921b2ff09533d6d62879cfc46f3
unknown <zkr.wangzm@YUNWEI.CNTAIPING.AD>
2020-06-29 16:18:15 +0800

    2

---
+++ spring-boot-demo-mq-rabbitmq/src/main/java/com/xkcoding/mq/rabbitmq/SpringBootDemoMqRabbitmqApplication.java
@@ -0,0 +1,17 @@
+package com.xkcoding.mq.rabbitmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zkr.wangzm
+ */
+@SpringBootApplication
+public class SpringBootDemoMqRabbitmqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDemoMqRabbitmqApplication.class, args);
+    }
+
+}
+
